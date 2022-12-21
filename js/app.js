64fc3a9ccf1a0690f7de538affbc47e3a512aaa0
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2022-12-22 08:55:41 +1100

    Fix paths and filenames.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 import * as THREE from 'https://cdn.skypack.dev/three@0.136.0';
-import {OrbitControls} from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls.js';
-import {GLTFLoader} from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/loaders/GLTFLoader.js';
 
 // Getting started / creating a scene using three.js
 // Reference: https://threejs.org/docs/index.html#manual/en/introduction/Creating-a-scene
@@ -10,11 +10,11 @@ import {GLTFLoader} from 'https://cdn.skypack.dev/three@0.136.0/examples/jsm/loa
 // Three.js Responsive Design
 // Reference: https://r105.threejsfundamentals.org/threejs/lessons/threejs-responsive.html
 
-function main(){
+function main() {
   // Declare the canvas element and instantiate the renderer.
   const canvas = document.querySelector('#c');
-  const renderer = new THREE.WebGLRenderer( {canvas} );
-  
+  const renderer = new THREE.WebGLRenderer({ canvas });
+
   // Instantiate a camera to view the model.
   const fov = 75;
   const aspect = window.innerWidth / window.innerHeight;
@@ -22,7 +22,7 @@ function main(){
   const far = 1000;
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
   camera.position.set(0.5, 0.25, 0.5);
-  
+
   // Instantiate a scene.
   const scene = new THREE.Scene();
 
@@ -41,22 +41,27 @@ function main(){
 
   // Define controls to manipulate the camera.
   const controls = new OrbitControls(camera, renderer.domElement);
-  controls.target.set(0,0,0);
+  controls.target.set(0, 0, 0);
   controls.update();
   controls.enablePan = false;
 
   // Load and animate the model.
   const loader = new GLTFLoader();
-  loader.load('./models/gltf/volkswagen-type3.glb', function(gltf) {
-    const car = gltf.scene;
-    console.log(car);
-    //car.children[24].visible = false; // hide the roof 
-    car.scale.set(0.15, 0.15, 0.15);
-    scene.add(car);
-    render();
-  }, undefined, function(error) {
-    console.error(error);
-  });
+  loader.load(
+    './assets/models/volkswagen-type3.glb',
+    function (gltf) {
+      const car = gltf.scene;
+      console.log(car);
+      //car.children[24].visible = false; // hide the roof
+      car.scale.set(0.15, 0.15, 0.15);
+      scene.add(car);
+      render();
+    },
+    undefined,
+    function (error) {
+      console.error(error);
+    }
+  );
 
   function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -74,12 +79,12 @@ function main(){
   function render(time) {
     time *= 0.001;
 
-    if( resizeRendererToDisplaySize(renderer) ) {
+    if (resizeRendererToDisplaySize(renderer)) {
       const canvas = renderer.domElement;
       camera.aspect = canvas.clientWidth / canvas.clientHeight;
       camera.updateProjectionMatrix();
     }
-    
+
     renderer.render(scene, camera);
 
     requestAnimationFrame(render);
